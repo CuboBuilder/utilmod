af918c6cebda3cfdab7126d33c84bfa1de79c695
service <service@undefined.com>
2025-05-13 00:03:14 +0300

    Implement team switch functionality in ExampleJavaMod with chat command support

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -8,29 +8,53 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import mindustry.net.Administration.*;
 
-public class ExampleJavaMod extends Mod{
+public class ExampleJavaMod extends Mod {
+    private boolean teamSwitchActive = false;
+    private Timer.Task teamSwitchTask;
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public ExampleJavaMod() {
+        Log.info("Loaded TeamSwitch Mod.");
 
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
+        // Listen for chat messages
+        Events.on(PlayerChatEvent.class, event -> {
+            String message = event.message.toLowerCase();
+            
+            if (message.equals(".start teamswitch")) {
+                if (!teamSwitchActive) {
+                    startTeamSwitch();
+                    event.player.sendMessage("[green]Team switching started!");
+                }
+            } else if (message.equals(".stop teamswitch")) {
+                if (teamSwitchActive) {
+                    stopTeamSwitch();
+                    event.player.sendMessage("[red]Team switching stopped!");
+                }
+            }
         });
     }
 
-    @Override
-    public void loadContent(){
-        Log.info("Loading some example content.");
+    private void startTeamSwitch() {
+        teamSwitchActive = true;
+        teamSwitchTask = Timer.schedule(() -> {
+            if (teamSwitchActive) {
+                int randomTeam = (int)(Math.random() * 256);
+                Call.sendChatMessage("/team " + randomTeam);
+            }
+        }, 0f, 5f);
     }
 
+    private void stopTeamSwitch() {
+        teamSwitchActive = false;
+        if (teamSwitchTask != null) {
+            teamSwitchTask.cancel();
+            teamSwitchTask = null;
+        }
+    }
+
+    @Override
+    public void loadContent() {
+        Log.info("TeamSwitch Mod loaded.");
+    }
 }
